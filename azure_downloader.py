484746385e7598bb80529caaf806c783f4874466
abhiTronix <abhi.una12@gmail.com>
2020-09-17 20:04:31 +0530

    Critical FFmpeg permission Bugfix for Linux and OSX machines.

--- azure_downloader.py
+++ azure_downloader.py
@@ -113,6 +113,9 @@ if __name__ == "__main__":
     else:
         ffmpeg_location = ""
 
+    if not os_windows and not os.access(ffmpeg_location, os.X_OK):
+        os.chmod(ffmpeg_location, 509)
+
     ydl_opts = {
         "format": "bestvideo+bestaudio/best",
         "outtmpl": output + ".%(ext)s",
